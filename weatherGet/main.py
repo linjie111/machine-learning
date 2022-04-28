bcbbc7edc49bdab330fe705ec616c2cf13b2431e
Wang.Lin <2997854889@qq.com>
2022-04-28 15:02:41 +0800

    获取天气

---
+++ weatherGet/main.py
@@ -0,0 +1,21 @@
+from timeit import Timer
+
+import Weather
+import laugh
+
+city = Weather.city
+nowTime = Weather.nowTime
+weather = Weather.weather
+tem = Weather.tem
+notice = Weather.notice
+time = Weather.time
+
+
+print(city)
+print(nowTime)
+print(weather)
+print(tem)
+print(notice)
+print(time)
+
+
